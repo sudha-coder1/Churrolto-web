fad941bbf3fbdd896ee826e77b1c7618681f80a4
sudha <sudha.p@technovert.com>
2023-12-18 12:48:03 +0530

    changes in js

--- churro.js
+++ churro.js
@@ -22,22 +22,18 @@ function openNav() {
           counter++;
           carouselSlide.style.transform = `translateX(${-counter * cardWidth}px)`;
       } else {
-          // Reset to the beginning if at the end
           counter = 0;
           carouselSlide.style.transform = `translateX(0)`;
       }
   }
   
-  // Set interval for automatic transition every 3 seconds (adjust as needed)
   const intervalId = setInterval(nextSlide, 3000);
   
-  // Optionally, stop the automatic transition on mouseover
-  carouselSlide.addEventListener('mouseover', () => {
-      clearInterval(intervalId);
-  });
+//   carouselSlide.addEventListener('mouseover', () => {
+//       clearInterval(intervalId);
+//   });
   
-  // Optionally, resume the automatic transition on mouseout
-  carouselSlide.addEventListener('mouseout', () => {
-      intervalId = setInterval(nextSlide, 3000);
-  });
+//   carouselSlide.addEventListener('mouseout', () => {
+//       intervalId = setInterval(nextSlide, 3000);
+//   });
   
